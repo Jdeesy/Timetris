13334e9cf024b812ba636f093a8120841ee84061
John Dees <jdees08@gmail.com>
2015-05-08 07:36:21 -0500

    added controllers for my models

---
+++ spec/helpers/task_report_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+# Specs in this file have access to a helper object that includes
+# the TaskReportHelper. For example:
+#
+# describe TaskReportHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+RSpec.describe TaskReportHelper, type: :helper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
