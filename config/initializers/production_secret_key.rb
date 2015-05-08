09659f4eff6e954a51e8bc7550a57eabcba808ad
Alex Knoll <alexandergknoll@gmail.com>
2015-05-08 13:30:11 -0500

    testing

--- config/initializers/production_secret_key.rb
+++ config/initializers/production_secret_key.rb
@@ -1,3 +1,3 @@
 # Do not keep production secrets in the repository,
 # instead read values from the environment.
-Rails.application.config.secret_key_base = ENV["SECRET_KEY_BASE"]
+# Rails.application.config.secret_key_base = ENV["SECRET_KEY_BASE"]
