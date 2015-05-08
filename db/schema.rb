6eda5815e0080aa14d9fe2d97de494e5af819426
John Dees <jdees08@gmail.com>
2015-05-08 18:28:58 -0500

    added indexes, added the ability to create tasks with a default time

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150508023225) do
+ActiveRecord::Schema.define(version: 20150508232456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,10 +26,11 @@ ActiveRecord::Schema.define(version: 20150508023225) do
 
   create_table "tasks", force: :cascade do |t|
     t.integer  "user_id"
-    t.integer  "time_box"
+    t.integer  "time_box",   default: 15
     t.datetime "due_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.string   "name"
   end
 
   create_table "users", force: :cascade do |t|
