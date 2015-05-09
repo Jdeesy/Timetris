417b68ccb518c66c517eae5ad24a56fdf1d3ca61
Jdeesy <Jdees08@gmail.com>
2015-05-09 13:51:46 -0500

    getting some secretes

--- app/controllers/task_reports_controller.rb
+++ app/controllers/task_reports_controller.rb
@@ -1,5 +1,14 @@
 class TaskReportsController < ApplicationController
   def index
-    @taskreports = current_user.task_reports.select{ |report| report.end_time }
+    @taskreports = current_user.task_reports.select{ |report| report.end_time }.includes(:task)
+  end
+
+  def patch
+    taskreport = TaskReport.find_by(id: params[:id])
+    taskreport.update(start_time: Time.now.utc)
+    redirect_to taskreport
+  end
+
+  def show
   end
 end
