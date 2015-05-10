03d496f4f7be94ad26af6528d5d8d4152767f13b
Alex Knoll <alexandergknoll@gmail.com>
2015-05-10 15:20:02 -0500

    updated gemfile, revise factory girl support files

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -2,26 +2,30 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 end
 
+# Test::Unit
+class Test::Unit::TestCase
+  include FactoryGirl::Syntax::Methods
+end
 
-FactoryGirl.define do
-  factory :user do
-    provider         "google"
-    uid              "universal id"
-    email            "john.doe@gmail.com"
-    name             "John Doe"
-    oauth_token      "oauth token"
-    oauth_expires_at Time.now
-    refresh_token    "refresh token"
-  end
+# Cucumber
+World(FactoryGirl::Syntax::Methods)
 
-  factory :task do
-    association :creator, factory: :user, name: "John Doe"
-    name        "Do cool stuff"
-    time_box    15   
-    due_date    Time.now
-    start_time  Time.now
-    end_time    Time.now
-    event_id    "google event id"
-    priority    2
-  end
+# Spinach
+class Spinach::FeatureSteps
+  include FactoryGirl::Syntax::Methods
+end
+
+# MiniTest
+class MiniTest::Unit::TestCase
+  include FactoryGirl::Syntax::Methods
+end
+
+# MiniTest::Spec
+class MiniTest::Spec
+  include FactoryGirl::Syntax::Methods
+end
+
+# minitest-rails
+class MiniTest::Rails::ActiveSupport::TestCase
+  include FactoryGirl::Syntax::Methods
 end
