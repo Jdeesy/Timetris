d8fd974af6c8c85a4c87d45e3277880f40baf9fc
John Dees <jdees08@gmail.com>
2015-05-10 15:14:38 -0500

    pushing factory girl creations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,38 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let!( :user ){ User.create!(provider: "provider_string", uid: "user_id_string", name: "John", oauth_token: "token_string", oauth_expires_at: Time.now) }
-  let!( :task ){ Task.create!(creator: user, name: 'task', due_date: Time.now, start_time: Time.now, end_time: Time.now) }
+  user = FactoryGirl.create(:user)
+  # let!( :user ){ User.create!(provider: "provider_string", uid: "user_id_string", name: "John", oauth_token: "token_string", oauth_expires_at: Time.now) }
+  # let!( :task ){ Task.create!(creator: user, name: 'task', due_date: Time.now, start_time: Time.now, end_time: Time.now) }
   
   describe "User Model Methods" do
     it 'should return a user id' do
       expect(user.id).to be_a(Fixnum)
     end
 
-    it 'should return a user provider' do
-      expect(user.provider).to eq("provider_string")
-    end
+    # it 'should return a user provider' do
+    #   expect(user.provider).to eq("provider_string")
+    # end
 
-    it 'should return a user uid' do
-      expect(user.uid).to eq("user_id_string")
-    end
+    # it 'should return a user uid' do
+    #   expect(user.uid).to eq("user_id_string")
+    # end
 
-    it 'should return a user name' do
-      expect(user.name).to eq("John")
-    end
+    # it 'should return a user name' do
+    #   expect(user.name).to eq("John")
+    # end
 
-    it 'should return a user oauth_token' do
-      expect(user.oauth_token).to eq("token_string")
-    end
+    # it 'should return a user oauth_token' do
+    #   expect(user.oauth_token).to eq("token_string")
+    # end
 
-    it 'should return a user oauth_expires_at' do
-      expect(user.oauth_expires_at).to be_a(Time)
-    end
+    # it 'should return a user oauth_expires_at' do
+    #   expect(user.oauth_expires_at).to be_a(Time)
+    # end
   end
 
   describe "User Relationships" do 
-    it 'should return the user' do
-      expect(user.tasks[0].creator).to eq(user)
-    end
+    # it 'should return the user' do
+    #   expect(user.tasks[0].creator).to eq(user)
+    # end
   end
 end
---
+++ spec/support/factory_girl.rb
@@ -0,0 +1,27 @@
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+end
+
+
+FactoryGirl.define do
+  factory :user do
+    provider         "google"
+    uid              "universal id"
+    email            "john.doe@gmail.com"
+    name             "John Doe"
+    oauth_token      "oauth token"
+    oauth_expires_at Time.now
+    refresh_token    "refresh token"
+  end
+
+  factory :task do
+    association :creator, factory: :user, name: "John Doe"
+    name        "Do cool stuff"
+    time_box    15   
+    due_date    Time.now
+    start_time  Time.now
+    end_time    Time.now
+    event_id    "google event id"
+    priority    2
+  end
+end
