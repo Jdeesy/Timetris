48d8829fc4e2daffdeeb3144d0722f3348ce2935
John Dees <jdees08@gmail.com>
2015-05-09 19:21:48 -0500

    fixed formatting and added priority

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150508013139) do
+ActiveRecord::Schema.define(version: 20150509235052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20150508013139) do
     t.string   "event_id"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+    t.integer  "priority",   default: 2
   end
 
   create_table "users", force: :cascade do |t|
