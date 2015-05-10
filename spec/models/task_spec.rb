7364824e3df2929a666182aec00e7917c7e64079
John Dees <jdees08@gmail.com>
2015-05-10 16:44:28 -0500

    added tests for tasks

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -9,6 +9,10 @@ RSpec.describe Task, type: :model do
       expect(task.id).to be_a(Fixnum)
     end
 
+    it 'should return a task name' do
+      expect(task.name).to eq("Do cool stuff")
+    end
+
     it 'should return a task time box' do
       expect(task.time_box).to eq(15)
     end
@@ -24,6 +28,34 @@ RSpec.describe Task, type: :model do
     it 'should return a time' do
       expect(task.end_time).to be_a(Time)
     end
+
+    it 'should return a task event id' do
+      expect(task.event_id).to eq("google event id")
+    end
+
+    it 'should return a task priority' do
+      expect(task.priority).to eq(2)
+    end
+
+    it 'should return a task target time' do
+      expect(task.target_finish_time).to eq(Time.at(task.start_time).utc + (task.time_box * 60))
+    end
+
+    it 'should return a task time' do
+      expect(task.task_time).to eq(0)
+    end
+
+    it 'should return the difference between task time and time box' do
+      expect(task.difference).to eq(15)
+    end
+
+    it 'should return the difference between task time and time box in words' do
+      expect(task.time_box_difference).to eq("15 minutes under")
+    end
+
+    it 'should return false since no task is in progress' do
+      expect(task.task_in_progress).to be false
+    end            
   end
 
   describe "task Relationships" do 
