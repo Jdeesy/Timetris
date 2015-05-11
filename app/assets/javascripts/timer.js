02246c744f650db98d5298e3bf53c58e6e5155fc
CJ Wilson <christopher.wilson.jr@gmail.com>
2015-05-11 13:32:13 -0500

    add basic timer functionality

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -1,50 +1,36 @@
+var task;
+
 $(document).ready(function(){
   var request = $.ajax({
-    url: "/tasks/45",
+    url: "/tasks/49",
     method: "get",
-    // dataType: "json",
-    // data: $this.serialize()
   }).done(function(response){
-    var startTime = response.start_time;
-    var timeBox = response.time_box
-  //   console.log("@task")
-  var now = new Date();
-  var now_utc = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(),  now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
-
-    var start = new Date(startTime);
-
-    console.log(now_utc)
-    console.log(start)
-    // console.log(Time.now())
-    // console.log(startTime);
-    // console.log(response);
-    // console.log(timeBox);
+    task = response;
+    getEachSecond();
   })
-  // })
-
-    // console.log(JSON.parse(data));
-
-  // console.log(data)
-  // var task = task.evalJSON();
-  // console.log("hello")
-
-
 })
 
-// <script>
-// function startTime() {
-//     var today=new Date();
-//     var h=today.getHours();
-//     var m=today.getMinutes();
-//     var s=today.getSeconds();
-//     m = checkTime(m);
-//     s = checkTime(s);
-//     document.getElementById('txt').innerHTML = h+":"+m+":"+s;
-//     var t = setTimeout(function(){startTime()},500);
-// }
+function getEachSecond() {
+     window.setTimeout(getEachSecond, 1000);
 
-// function checkTime(i) {
-//     if (i<10) {i = "0" + i};  // add zero in front of numbers < 10
-//     return i;
-// }
-// </script>
+     calculateTime(task);
+ }
+
+ function calculateTime(response){
+    var startTime = response.start_time;
+    var timeBox = response.time_box
+    var now = new Date();
+    var start = new Date(startTime);
+    var timeDifference = Math.floor(((now - start)/1000))
+
+    var hours = Math.floor(timeDifference/3600);
+
+    var minutes = Math.floor((timeDifference/60) % 60);
+
+    var seconds = Math.floor(timeDifference % 60);
+
+    $(".hours").html(hours);
+    $(".minutes").html(minutes);
+    $(".seconds").html(seconds);
+
+  }
