c23f6906b77c652ecb26f15f52dc492b0e532278
Alex Knoll <alexandergknoll@gmail.com>
2015-05-10 19:06:39 -0500

    hooray time zone helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,19 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  helper_method :current_user
 
+  protect_from_forgery with: :exception
   before_action :require_login, :require_completed_task
+  helper_method :current_user, :format_local_time
+
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def format_local_time(time)
+    return Time.at(time).in_time_zone.strftime("%A, %b %e at %l:%M %p")
+  end
+
+  private
 
   def require_login
     redirect_to welcome_path unless current_user
