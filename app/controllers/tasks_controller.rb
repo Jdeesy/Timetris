597ac55c8cc653c8942ce8dc7d0021992df90cc3
CJ Wilson <christopher.wilson.jr@gmail.com>
2015-05-12 09:47:07 -0500

    add description to task, ability to add task from show page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -98,7 +98,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:name, :due_date)
+    params.require(:task).permit(:name, :due_date, :description)
   end
 
 end
